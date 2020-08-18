012f13a5473011643715467c96774cf68fc5c0d0
Jesús Reyes <javier.reyes.ic@gmail.com>
2020-08-17 21:20:00 -0500

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -9,3 +9,15 @@
 def add_position(x):
     x['position'] = np.arange(1, len(x) + 1)
     return x
+
+""" complete a serie of dates """
+
+# year_month: string of the date, for example '2019-01'
+# n: number of months to add to the lower-bound year_month
+
+lower_bound_period = year_month
+lower_bound_period = pd.Period(lower_bound_period, 'M')
+    
+months = []
+for i in range(n):
+    months.append( str(max_mnt + pd.offsets.MonthEnd( i + 1)) ) # save in string datatype
