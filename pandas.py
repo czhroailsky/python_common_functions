6b05bad23820792cf9887d0553dab5793f09c779
Jesús Reyes <javier.reyes.ic@gmail.com>
2020-08-31 13:35:10 -0500

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -21,3 +21,20 @@ lower_bound_period = pd.Period(lower_bound_period, 'M')
 months = []
 for i in range(n):
     months.append( str(max_mnt + pd.offsets.MonthEnd( i + 1)) ) # save in string datatype
+
+   
+## X is a pandas dataframe with the columns to compute the correlation matrix, with plotly
+
+import plotly.figure_factory as ff
+
+corrs = X.corr()
+
+fig = ff.create_annotated_heatmap(
+    z=corrs.values,
+    x=list(corrs.columns),
+    y=list(corrs.index),
+    annotation_text=corrs.round(2).values,
+    showscale=True
+)
+
+fig.show()
